41296f8051e29fd7e905d9707995f65ec6f36e45
khemisse-radwane <khemisse.radwane@gmail.com>
2024-11-04 23:18:04 +0100

    Create TropViteException.java

---
+++ TP3/EXo1/TropViteException.java
@@ -0,0 +1,7 @@
+package EXo1;
+
+public class TropViteException extends Exception {
+    public TropViteException(int vitesse) {
+        super("C'est une exception de type TropViteException. Vitesse en cause : " + vitesse);
+    }
+}
